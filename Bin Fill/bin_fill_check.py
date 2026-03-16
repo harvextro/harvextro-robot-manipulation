fd38dab0f33d416feb609c47b927b633d04a2937
Supuni Wijesinghe <supuni.20240708@iit.ac.lk>
2026-03-16 08:01:04 +0530

    Added new bin fill check

---
+++ Bin Fill/bin_fill_check.py
@@ -0,0 +1,45 @@
+from machine import Pin
+import time
+
+# Define pins
+trig = Pin(5, Pin.OUT)
+echo = Pin(18, Pin.IN)
+
+def measure_distance():
+    # Send pulse
+    trig.off()
+    time.sleep_us(2)
+    trig.on()
+    time.sleep_us(10)
+    trig.off()
+
+    # Wait for echo start
+    while echo.value() == 0:
+        pass
+    start = time.ticks_us()
+
+    # Wait for echo end
+    while echo.value() == 1:
+        pass
+    end = time.ticks_us()
+
+    duration = time.ticks_diff(end, start)
+    
+    # Speed of sound formula
+    distance = (duration * 0.0343) / 2
+    return distance
+
+
+BIN_HEIGHT = 30   
+FULL_THRESHOLD = 5  
+
+while True:
+    distance = measure_distance()
+    print("Distance:", distance, "cm")
+
+    if distance <= FULL_THRESHOLD:
+        print("⚠️ BIN FULL!")
+    else:
+        print("Bin not full")
+
+    time.sleep(2)
